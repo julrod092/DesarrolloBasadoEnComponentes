3314a4294c0d3fda3bd2d2f5ad513d40480ebf40
Julian Rodriguez <julrod092@gmail.com>
2016-04-17 22:58:54 -0500

    Practica 1 de la materia Desarrollo basado en componentes

---
+++ README.md
@@ -0,0 +1,16 @@
+# Repositorio para practicas de la materia Desarollo Basado en Componentes
+
+## Practica 1: HATEOAS (Hypermedia as the Engine of Application State)
+
+* Lenguaje de desarrollo: Ruby
+* API utilizada: [GitHub API] de [GitHub Developer Site]
+
+### Comandos
+```sh
+$ ruby practica1.rb
+```
+
+
+
+[GitHub API]: <https://api.github.com/>
+[GitHub Developer Site]: <https://developer.github.com>
---
+++ practica1/practica1.rb
@@ -0,0 +1,72 @@
+require 'open-uri'
+require 'json'
+
+class Practica1
+
+
+  @@url = ''
+  @@profileName = ''
+
+  def initialize ()
+    puts "Please write a user to cloning a github repository"
+    user = gets.chomp
+    @@url = "https://api.github.com/users/#{user}"
+  end
+
+  def callGithubApi ()
+
+    begin
+      getResponseProfile = open(@@url)
+      profileJson = JSON.parse(getResponseProfile.read)
+      @@profileName = profileJson["login"]
+      getRepos(profileJson["repos_url"])
+    rescue OpenURI::HTTPError => error
+      response = error.io
+      at_exit do
+        puts "Error in request. status #{response.status[0]} :: message #{response.status[1]}"
+      end
+    end
+  end
+
+  def getRepos (url)
+    begin
+      getResponseRepos = open(url)
+      reposJson = JSON.parse(getResponseRepos.read)
+      puts "Github repositories availables with user #{@@profileName}. Select a number"
+      $i = 1
+      reposJson.each do |repo|
+        puts "#{$i} #{repo["name"]}"
+        $i += 1
+      end
+      ARGV.clear
+      repo = gets.chomp
+      repo = Integer(repo)
+      cloningRepo(reposJson[repo - 1])
+    rescue OpenURI::HTTPError => error
+      response = error.io
+      at_exit do
+        puts "Error in request (Getting repos). status #{response.status[0]} :: message #{response.status[1]}"
+      end
+    end
+  end
+
+  def cloningRepo (repoHash)
+    $folder = false
+    Dir.entries('./').select { |entry|
+      folder = (entry == repoHash["name"]) ? false : $folder = true
+    }
+    if $folder
+      exec("git clone #{repoHash['clone_url']} #{repoHash['name']}")
+    else
+      at_exit do
+        puts "Error cloning github repository. Folder destination exist"
+      end
+    end
+  end
+
+
+  private :getRepos, :cloningRepo
+end
+
+gitHubApi = Practica1.new()
+gitHubApi.callGithubApi()
